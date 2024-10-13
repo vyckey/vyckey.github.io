e5b7d40bc0d051ea7274d875c7e20a16e1702fe6
vyckey <vyckey@qq.com>
2024-10-13 15:32:56 +0800

    update

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,10 @@ import Homepage from '../pages/Homepage';
 
 const router = createBrowserRouter([
   {
+    path: '/',
+    element: <Homepage />,
+  },
+  {
     path: '/home',
     element: <Homepage />,
   },
